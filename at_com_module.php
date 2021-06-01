284e09a11ea17975bb98a6f0ed81a24d3dfb89cc
Dario Tecchia <dariotecchia@gmail.com>
2021-06-01 17:47:19 +0200

    fix typo and format code

--- at_com_module.php
+++ at_com_module.php
@@ -1,34 +1,34 @@
 <?php
 /**
-* 2007-2021 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2021 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
+ * 2007-2021 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2021 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
 
 if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class at_com_module extends Module
+class At_com_module extends Module
 {
     protected $config_form = false;
 
@@ -48,9 +48,9 @@ class at_com_module extends Module
         parent::__construct();
 
         $this->displayName = $this->l('@.com module');
-        $this->description = $this->l('Prestashop module built in house by @.com Srl.');
+        $this->description = $this->l('Prestashop Module developed for @.com.');
 
-        $this->confirmUninstall = $this->l('Are you sure you want to uninstall the module?');
+        $this->confirmUninstall = $this->l('');
 
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
     }
@@ -63,29 +63,20 @@ class at_com_module extends Module
     {
         Configuration::updateValue('AT_COM_MODULE_LIVE_MODE', false);
 
-        Configuration::updateValue('PS_B2B_ENABLE', true);
-
-        include(dirname(__FILE__).'/sql/install.php');
+        include dirname(__FILE__) . '/sql/install.php';
 
         return parent::install() &&
-            $this->registerHook('header') &&
-            $this->registerHook('backOfficeHeader');
-            // $this->registerHook('additionalCustomerFormFields') &&
-            // $this->registerHook('validateCustomerFormFields');
+        $this->registerHook('header') &&
+        $this->registerHook('backOfficeHeader');
     }
 
     public function uninstall()
     {
         Configuration::deleteByName('AT_COM_MODULE_LIVE_MODE');
 
-        include(dirname(__FILE__).'/sql/uninstall.php');
+        include dirname(__FILE__) . '/sql/uninstall.php';
 
         return parent::uninstall();
-    }
-
-    public function isUsingNewTranslationSystem()
-    {
-        return true;
     }
 
     /**
@@ -96,15 +87,15 @@ class at_com_module extends Module
         /**
          * If values have been submitted in the form, process.
          */
-        if (((bool)Tools::isSubmit('submitAt_comModule')) == true) {
+        if (((bool) Tools::isSubmit('submitAt_com_moduleModule')) == true) {
             $this->postProcess();
         }
 
         $this->context->smarty->assign('module_dir', $this->_path);
 
-        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+        $output = $this->context->smarty->fetch($this->local_path . 'views/templates/admin/configure.tpl');
 
-        return $output.$this->renderForm();
+        return $output . $this->renderForm();
     }
 
     /**
@@ -121,9 +112,9 @@ class at_com_module extends Module
         $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
 
         $helper->identifier = $this->identifier;
-        $helper->submit_action = 'submitAt_comModule';
+        $helper->submit_action = 'submitAt_com_moduleModule';
         $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
-            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
         $helper->token = Tools::getAdminTokenLite('AdminModules');
 
         $helper->tpl_vars = array(
@@ -143,8 +134,8 @@ class at_com_module extends Module
         return array(
             'form' => array(
                 'legend' => array(
-                'title' => $this->l('Settings'),
-                'icon' => 'icon-cogs',
+                    'title' => $this->l('Settings'),
+                    'icon' => 'icon-cogs',
                 ),
                 'input' => array(
                     array(
@@ -157,13 +148,13 @@ class at_com_module extends Module
                             array(
                                 'id' => 'active_on',
                                 'value' => true,
-                                'label' => $this->l('Enabled')
+                                'label' => $this->l('Enabled'),
                             ),
                             array(
                                 'id' => 'active_off',
                                 'value' => false,
-                                'label' => $this->l('Disabled')
-                            )
+                                'label' => $this->l('Disabled'),
+                            ),
                         ),
                     ),
                     array(
@@ -212,13 +203,13 @@ class at_com_module extends Module
     }
 
     /**
-    * Add the CSS & JavaScript files you want to be loaded in the BO.
-    */
+     * Add the CSS & JavaScript files you want to be loaded in the BO.
+     */
     public function hookBackOfficeHeader()
     {
         if (Tools::getValue('module_name') == $this->name) {
-            $this->context->controller->addJS($this->_path.'views/js/back.js');
-            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+            $this->context->controller->addJS($this->_path . 'views/js/back.js');
+            $this->context->controller->addCSS($this->_path . 'views/css/back.css');
         }
     }
 
@@ -227,84 +218,7 @@ class at_com_module extends Module
      */
     public function hookHeader()
     {
-        $this->context->controller->addJS($this->_path.'/views/js/front.js');
-        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+        $this->context->controller->addJS($this->_path . '/views/js/front.js');
+        $this->context->controller->addCSS($this->_path . '/views/css/front.css');
     }
-
-    /**
-     * Read Module fields values
-     *
-     * @return array of module value
-     */
-    // protected function readModuleValues()
-    // {
-    //     $id_customer = Context::getContext()->customer->id;
-    //     $query = 'SELECT c.`sdi`'
-    //         .' FROM `'. _DB_PREFIX_.'customer` c '
-    //         .' WHERE c.id_customer = '.(int)$id_customer;
-    //     return  Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow($query);
-    // }
-
-    /**
-     * Write module fields values
-     *
-     * @return nothing
-     */
-    // protected function writeModuleValues($id_customer)
-    // {
-    //     $sdi = Tools::getValue('sdi');
-    //     $query = 'UPDATE `'._DB_PREFIX_.'customer` c '
-    //         .' SET  c.`sdi` = "'.pSQL($sdi).'"'
-    //         .' WHERE c.id_customer = '.(int)$id_customer;
-    //     Db::getInstance()->execute($query);
-    // }
-
-    /**
-     * Add fields in Customer Form
-     *
-     * @param array $params parameters (@see CustomerFormatter->getFormat())
-     *
-     * @return array of extra FormField
-     */
-    // public function hookAdditionalCustomerFormFields($params)
-    // {
-    //     $module_fields = $this->readModuleValues();
-
-    //     $sdi_value = Tools::getValue('sdi');
-    //     if (isset($module_fields['sdi'])) {
-    //         $sdi_value = $module_fields['sdi'];
-    //     }
-
-    //     $extra_fields = array();
-    //     $extra_fields['sdi'] = (new FormField)
-    //         ->setName('sdi')
-    //         ->setType('text')
-    //         ->setValue($sdi_value)
-    //         ->setLabel($this->l('SDI'));
-
-    //     return $extra_fields;
-    // }
-
-    /**
-     * Validate fields in Customer form
-     *
-     * @param array $params hook call parameters (@see CustomerForm->validateByModules())
-     *
-     * @return array of extra FormField
-     */
-    // public function hookvalidateCustomerFormFields($params)
-    // {
-    //     $module_fields = $params['fields'];
-    //     if ('Dance' != $module_fields[0]->getValue()
-    //         && 'Shopping' != $module_fields[0]->getValue()
-    //         && 'Yoga' != $module_fields[0]->getValue()
-    //     ) {
-    //         $module_fields[0]->addError(
-    //             $this->l('Only "Dance", "Shopping" or "Yoga"')
-    //         );
-    //     }
-    //     return array(
-    //         $module_fields
-    //     );
-    // }
 }
--- override/controllers/front/CartController.php
+++ override/controllers/front/CartController.php
@@ -2,12 +2,12 @@
 
 class CartController extends CartControllerCore
 {
-  public function init()
-  {
-    $default_currency = intval(Configuration::get('PS_DEFAULT_CURRENCY'));
-    $this->currency = new Currency($default_currency);
-    $this->context->cookie->id_currency = $default_currency;
-    
-    parent::init();
-  }
+    public function init()
+    {
+        $default_currency = intval(Configuration::get('PS_DEFAULT_CURRENCY'));
+        $this->currency = new Currency($default_currency);
+        $this->context->cookie->id_currency = $default_currency;
+
+        parent::init();
+    }
 }
