fa46637800f334ae9802df8a1bb217b04e7a0ad3
Dario Tecchia <dariotecchia@gmail.com>
2021-06-24 18:57:22 +0200

    cart override init

---
+++ override/classes/Cart.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Class Cart
+ */
+class Cart extends CartCore
+{
+    /**
+     * Cart constructor.
+     *
+     * @param int|null $id
+     */
+    public function __construct($id = null)
+    {
+        parent::__construct($id);
+    }
+
+    private function volumeReducer($carry, $item)
+    {
+        $carry += ( (double) $item["width"] * (double) $item["height"] * (double) $item["depth"]) * (int) $item["cart_quantity"];
+        return $carry;
+    }
+
+    public function getCartVolume()
+    {
+        return array_reduce(parent::getProducts(), array("Cart", "volumeReducer"));
+    }
+
+    public function getCartPallets($palletCapacity = null)
+    {
+        if(is_null($palletCapacity)) {
+            $palletCapacity = (int) Configuration::get('AT_COM_MODULE_PALLET_CAP', 20);
+        }
+        return (int) ceil($this->getCartVolume() / $palletCapacity);
+    }
+
+}
