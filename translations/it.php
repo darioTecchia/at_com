8c4c3651856d216241de882f874bcad79616fe61
Dario Tecchia <dariotecchia@gmail.com>
2021-06-25 11:17:42 +0200

    init translation

---
+++ translations/it.php
@@ -0,0 +1,7 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{at_com_module}prestashop>cartfooter_b466403b03f7aa97c16de8beb7b6de35'] = 'Informazioni Pallet';
+$_MODULE['<{at_com_module}prestashop>cartfooter_0886bc3db6d54fa42f722e4d1b486096'] = 'Volume totale della spedizione';
+$_MODULE['<{at_com_module}prestashop>cartfooter_e014bbc5bd6a50fc71f964603a2d29da'] = 'Numero approssimativo di pallet';
