dd2c9035c03ca1f9ea69fb53d1a19f0396cf04e6
Dario Tecchia <dariotecchia@gmail.com>
2021-08-12 15:35:51 +0200

    force currency change in order page

---
+++ override/controllers/front/OrderController.php
@@ -0,0 +1,13 @@
+<?php
+
+class OrderController extends OrderControllerCore
+{
+    public function init()
+    {
+        $default_currency = intval(Configuration::get('PS_DEFAULT_CURRENCY'));
+        $this->currency = new Currency($default_currency);
+        $this->context->cookie->id_currency = $default_currency;
+
+        parent::init();
+    }
+}
